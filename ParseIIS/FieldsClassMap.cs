4f5b87d90432f7749f251a52fa04d8b80b69d505
John Best <jbest145@icloud.com>
2018-11-26 21:47:46 -0500

    Initial Commit for log analyzer project

---
+++ ParseIIS/FieldsClassMap.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseIIS
+{
+   public class FieldsClass
+    {
+        public string IP { get; set; }
+        public int IPCount { get; set; }
+    }
+    public sealed class LogClassMap : ClassMap<FieldsClass>
+    {
+        public LogClassMap()
+        {
+            Map(d => d.IPCount);
+            Map(d => d.IP).ConvertUsing(m => $"\"{m.IP}\"");
+
+        }
+    }
+}
